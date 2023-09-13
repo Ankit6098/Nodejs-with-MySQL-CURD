ee4ad2b12e5d409ec208cdf0b9f6119e9ed411b9
Ankit <ankitvis609@gmail.com>
2023-09-13 18:09:25 +0530

    create database if not exit

--- config/mysql.js
+++ config/mysql.js
@@ -4,15 +4,49 @@ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'my_nodejs_crud'
 });
 
-connection.connect(err => {
+// Check if the database exists; if not, create it
+connection.connect((err) => {
   if (err) {
-    console.error('Error connecting to MySQL:', err);
+    console.error('Error connecting to MySQL: ' + err.stack);
     return;
   }
-  console.log('Connected to MySQL Database');
+  console.log('Connected to MySQL as id ' + connection.threadId);
+
+  // Create the database if it doesn't exist
+  connection.query('CREATE DATABASE IF NOT EXISTS my_nodejs_MySQL_crud', (createDbErr) => {
+    if (createDbErr) {
+      console.error('Error creating database: ' + createDbErr.message);
+    } else {
+      console.log('Database created or already exists');
+    }
+
+    // Use the created database
+    connection.query('USE my_nodejs_MySQL_crud', (useDbErr) => {
+      if (useDbErr) {
+        console.error('Error using database: ' + useDbErr.message);
+      } else {
+        console.log('Using database: my_nodejs_MySQL_crud');
+
+        // Create the table if it doesn't exist
+        connection.query(
+          `CREATE TABLE IF NOT EXISTS todos (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            task VARCHAR(255) NOT NULL,
+            completed BOOLEAN DEFAULT 0
+          )`,
+          (createTableErr) => {
+            if (createTableErr) {
+              console.error('Error creating table: ' + createTableErr.message);
+            } else {
+              console.log('Table created or already exists');
+            }
+          }
+        );
+      }
+    });
+  });
 });
 
 module.exports = connection;
